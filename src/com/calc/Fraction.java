d3a45c4c5b4b87b4e2d58500d82d042becd26088
tsiganskytabor <makskirich03@mail.ru>
2022-02-26 21:15:45 +0300

    Add files

---
+++ src/com/calc/Fraction.java
@@ -0,0 +1,135 @@
+package com.calc;
+
+class Fraction {
+
+    private int num, denom;
+
+    Fraction() {
+        this.num = 1;
+        this.denom = 1;
+    }
+
+    Fraction(int num, int denom) {
+        if (denom == 0) {
+            System.out.println("ОШИБКА: Деление на ноль");
+            System.out.println("        Пожалуйста, повторите попытку");
+            System.exit(1);
+        } else {
+            this.num = num;
+            this.denom = denom;
+            this.reductionValue();
+        }
+    }
+
+//    void setValue(int num, int denom) {
+//        if (denom == 0) {
+//            System.out.println("ОШИБКА: Деление на ноль");
+//            System.out.println("        Пожалуйста, повторите попытку");
+//            System.exit(1);
+//        } else {
+//            this.num = num;
+//            this.denom = denom;
+//            this.reductionValue();
+//        }
+//    }
+
+    String getValue() {
+        String value;
+        if (this.num == this.denom) {
+            return "1";
+        } else if (this.denom == 1) {
+            return this.num + "";
+        } else if (this.num == 0) {
+            return "0";
+        } else if (this.denom < 0) {
+            return(this.num * -1) + "/" + (this.denom * -1);
+        } else {
+            return this.num + "/" + this.denom;
+        }
+    }
+
+    private void reductionValue() {
+        if (this.num == this.denom) {
+            this.num = 1;
+            this.denom = 1;
+        } else if (this.num == 0) {
+            this.denom = 1;
+        } else {
+            int gcd = greatestCommonDivisor(this.num, this.denom);
+            this.num /= gcd;
+            this.denom /= gcd;
+        }
+    }
+
+    private int greatestCommonDivisor(int first, int second) {
+        int gcd = 1;
+        int minValue = Math.min(Math.abs(first), Math.abs(second));
+        for (int i = minValue; i >= 2; i--) {
+            if ((Math.abs(first) % i == 0) && (Math.abs(second) % i == 0)) {
+                gcd = i;
+                break;
+            }
+        }
+        return gcd;
+    }
+
+    void summation(Fraction summand) {
+        this.num = this.num * summand.denom + summand.num * this.denom;
+        this.denom *= summand.denom;
+        reductionValue();
+    }
+
+    void summation(Fraction first, Fraction second) {
+        this.num = first.num * second.denom + second.num * first.denom;
+        this.denom = first.denom * second.denom;
+        reductionValue();
+    }
+
+    void substraction(Fraction deductible) {
+        this.num = this.num * deductible.denom - deductible.num * this.denom;
+        this.denom *= deductible.denom;
+        reductionValue();
+    }
+
+    void substraction(Fraction first, Fraction second) {
+        this.num = first.num * second.denom - second.num * first.denom;
+        this.denom = first.denom * second.denom;
+        reductionValue();
+    }
+
+    void multiplication(Fraction multiplier) {
+        this.num *= multiplier.num;
+        this.denom *= multiplier.denom;
+        reductionValue();
+    }
+
+    void multiplication(Fraction first, Fraction second) {
+        this.num = first.num * second.num;
+        this.denom = first.denom * second.denom;
+        reductionValue();
+    }
+
+    void division(Fraction divisible) {
+        if (divisible.num == 0) {
+            System.out.println("ОШИБКА: Деление на ноль");
+            System.out.println("        Пожалуйста, повторите попытку");
+            System.exit(1);
+        } else {
+            this.num *= divisible.denom;
+            this.denom *= divisible.num;
+            reductionValue();
+        }
+    }
+
+    void division(Fraction first, Fraction second) {
+        if (second.num == 0) {
+            System.out.println("ОШИБКА: Деление на ноль");
+            System.out.println("        Пожалуйста, повторите попытку");
+            System.exit(1);
+        } else {
+            this.num = first.num * second.denom;
+            this.denom = first.denom * second.num;
+            reductionValue();
+        }
+    }
+}
---
+++ src/com/calc/Main.java
@@ -0,0 +1,69 @@
+package com.calc;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("*** КАЛЬКУЛЯТОР ДРОБЕЙ ***");
+        System.out.println();
+        System.out.println("Пожалуйста, следуйте инструкции для корректной работы программы:");
+        System.out.println("    - используйте цифры для ввода значений числителя и знаменателя дроби");
+        System.out.println("    - используйте '/' для обозначения черты деления дроби");
+        System.out.println("    - используйте '+', '-', '*' и '/' для обозначения арифметических операций");
+        System.out.println("    - ставьте пробелы между дробью и знаком арифметической операции");
+        System.out.println("    Пример ввода: 1/2 + 3/4");
+        System.out.println();
+        System.out.print("Введите выражение: ");
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        Pattern correctEnterPattern = Pattern.compile("(\\d+)/(\\d+)\\s[\\+\\-\\*\\/]\\s(\\d+)/(\\d+)");
+        Matcher correctEnterMatcher = correctEnterPattern.matcher(userInput);
+        if (!(correctEnterMatcher.matches())) {
+            System.out.println("ОШИБКА: Неверный ввод");
+            System.out.println("        Пожалуйста, повторите попытку");
+            System.exit(1);
+        } else {
+            String[] expression = userInput.split("\\s");
+
+            String[] firstFraction = expression[0].split("/");
+            int firstNum = Integer.parseInt(firstFraction[0]);
+            int firstDenom = Integer.parseInt(firstFraction[1]);
+            Fraction first = new Fraction(firstNum, firstDenom);
+
+            String operation = expression[1];
+
+            String[] secondFraction = expression[2].split("/");
+            int secondNum = Integer.parseInt(secondFraction[0]);
+            int secondDenom = Integer.parseInt(secondFraction[1]);
+            Fraction second = new Fraction(secondNum, secondDenom);
+
+            Fraction result = new Fraction();
+            switch (operation) {
+                case "+" -> {
+                    result.summation(first, second);
+                    first.summation(second);
+                }
+                case "-" -> {
+                    result.substraction(first, second);
+                    first.substraction(second);
+                }
+                case "*" -> {
+                    result.multiplication(first, second);
+                    first.multiplication(second);
+                }
+                case "/" -> {
+                    result.division(first, second);
+                    first.division(second);
+                }
+            }
+            System.out.println("Ответ:");
+            System.out.print("способ подсчета 1: ");
+            System.out.println(result.getValue());
+            System.out.print("способ подсчета 2: ");
+            System.out.println(first.getValue());
+        }
+    }
+}
